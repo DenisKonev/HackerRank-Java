6c42b62870f25c03eaeb9ff22bcc2c07c093313c
Denis Konev <deniskonev1984@gmail.com>
2023-08-16 17:28:45 +0300

    "Java Output Formatting" beta version

--- Java Output Formatting/src/Main.java
+++ Java Output Formatting/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String[] firstLine = scanner.nextLine().split(" ");
+        String[] secondLine = scanner.nextLine().split(" ");
+        String[] thirdLine = scanner.nextLine().split(" ");
+
+        scanner.close();
+
+        System.out.println("================================");
+        System.out.println(String.format("%-15s", firstLine[0]) + String.format("%03d", Integer.parseInt(firstLine[1])));
+        System.out.println(String.format("%-15s", secondLine[0]) + String.format("%03d", Integer.parseInt(secondLine[1])));
+        System.out.println(String.format("%-15s", thirdLine[0]) + String.format("%03d", Integer.parseInt(thirdLine[1])));
+        System.out.println("================================");
     }
 }
