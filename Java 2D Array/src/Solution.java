bdb91eacc81d33680f5559465c2928f9a274eb85
Denis Konev <deniskonev1984@gmail.com>
2023-09-10 11:37:23 +0300

    "Java 2D Array" solved

---
+++ Java 2D Array/src/Solution.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+        bufferedReader.close();
+
+        System.out.println(calcHourGlassMaxSum(arr));
+    }
+    private static int calcHourGlassMaxSum(List<List<Integer>> arr) {
+        int maxSum = Integer.MIN_VALUE, currentSum = 0;
+
+        for (int i = 0; i < arr.size() - 2; i++ )
+            for (int j = 0; j < arr.size() - 2; j++) {
+                currentSum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) + arr.get(i + 1).get(j + 1) + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                if (currentSum > maxSum)
+                    maxSum = currentSum;
+            }
+        return maxSum;
+    }
+}
